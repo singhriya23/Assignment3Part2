e756ef667a5204754744e99b4b88e51671dcbcb0
Riya Singh <singh.riya2@northeastern.edu>
2025-02-27 18:31:22 -0500

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"TOM_DB"."{{env}}_SCHEMA"."{{env}}_test"')
-    FROM '@"TOM_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/{{branch}}/'
+    FROM '@"TOM_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/{{branch}}/scripts'
     QUERY_WAREHOUSE = 'TOM_WH'
     MAIN_FILE = 'test.py';
