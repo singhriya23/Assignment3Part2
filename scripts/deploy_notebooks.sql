4459549d54dfd6f1be10e0a60ea88e44a5527f2c
Riya Singh <singh.riya2@northeastern.edu>
2025-02-28 00:12:29 -0500

    Update deploy_notebooks.sql
    Added for prod as well

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -4,3 +4,11 @@ CREATE OR REPLACE NOTEBOOK IDENTIFIER('"TOM_DB"."{{env}}_SCHEMA"."{{env}}_load_r
     MAIN_FILE = 'load_raw.py';
 
 ALTER NOTEBOOK "TOM_DB"."{{env}}_SCHEMA"."{{env}}_load_raw" ADD LIVE VERSION FROM LAST;
+
+/* 
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"TOM_DB"."{{env}}_SCHEMA"."{{env}}_load_raw"')
+    FROM '@"TOM_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/{{branch}}'
+    QUERY_WAREHOUSE = 'TOM_WH'
+    MAIN_FILE = 'load_raw.py';
+
+ALTER NOTEBOOK "TOM_DB"."{{env}}_SCHEMA"."{{env}}_load_raw" ADD LIVE VERSION FROM LAST; */
